74b1a7079da100083a67e0f7de9c02aa18613934
Hugo Juillet <hugo.juillet@endress.com>
2024-01-16 11:29:08 +0100

    Update packages

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,7 @@
 import os
+from kombu_fernet.serializers.json import MIMETYPE
+
 broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 celery_result_backend = os.getenv('REDIS_URL', 'redis://localhost:6379')
+task_serializer='fernet_json',
+accept_content=[MIMETYPE,'json','pickle','application/x-fernet-json']
